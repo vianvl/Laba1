aec95fe2eef502637c58442c4ba6f7b7839b9ab0
viktoriyaum <47516654+viktoriyaum@users.noreply.github.com>
2019-02-11 10:32:01 +0300

    Update README.cpp
    tosha petuh

--- README.cpp
+++ README.cpp
@@ -1,2 +1,23 @@
 #include <stdio.h>
-i
+int main (void)
+{
+  int a, b;
+  char c;
+  scanf ("%d/n", &a);
+  scanf ("%d/n", &b);
+  scanf ("%d/n", &c);
+  if(a>b){
+    printf("%d/n", c);
+           }
+         else(a<b)
+              {
+                printf("Anton moshet bit gandonom?/n Enter N and Y/n");//Namana shivem
+                scanf("%c", &c);
+                if(c==Y||c==y)
+                {
+                  printf ("Anton ne gandon");
+                }
+else 
+printf("You prav Anton ne gandon");
+  return 0;
+ }
