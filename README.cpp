62ba7278c2fe49966530bdf9409ddc0dd24ee486
Vladislav <47516232+MEDdoger99@users.noreply.github.com>
2019-02-11 10:30:00 +0300

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -3,16 +3,16 @@ int main (void)
 {
   int a, b;
   char c;
-  scanf ("%d/n", a);
-  scanf ("%d/n", b);
-  scanf ("%d/n", c);
+  scanf ("%d/n", &a);
+  scanf ("%d/n", &b);
+  scanf ("%d/n", &c);
   if(a>b){
-    printf("%d/n", c");
+    printf("%d/n", c);
            }
          else(a<b)
               {
-                printf("Anton moshet bit gandonom?/n Enter N and Y/n");
-                scanf("%c", c);
+                printf("Anton moshet bit gandonom?/n Enter N and Y/n");//Namana shivem
+                scanf("%c", &c);
                 if(c==Y||c==y)
                 {
                   printf ("Anton ne gandon");
