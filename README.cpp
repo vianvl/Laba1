a198076685eab04772c117e1ae6f2775e7fab8cb
Vladislav <47516232+MEDdoger99@users.noreply.github.com>
2019-02-21 20:07:29 +0000

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -2,15 +2,22 @@
 #include <iostream>
 #include<stdio.h>
 #include<math.h>
+#include<time.h>
+#include<ctype.h>
+#include<stdlib.h>
 #include <locale.h>
 #define FALSE 0
 #define TRUE 1
-//Прототипы функций дальше в теле буду прописывать функционал а какие то пропишу сразу
-char getRank(int rank); //РанГ карты 
-char getMast(int mast);//Масть карты 
-void getFirstHand(int cardRank[], int cardMast[]); //Функция выдачи карт 
+void printGreeting();
 int getBet();
-
+char getSuit(int suit);
+char getRank(int rank);
+void getFirstHand(int cardRank[], int cardSuit[]);
+void getFinalHand(int cardRank[], int cardSuit[], int finalRank[], int finalSuit[], int ranksinHand[], int suitsinHand[]);
+int analyzeHand(int ranksinHand[], int suitsinHand[]);
+int komb;
+int cheat;
+int start;
 char MAPA(int a)
 {
 	int a1;
@@ -37,22 +44,59 @@ int Cheat(int cheat, int Cash)
 	}
 	return S = 0;
 }
-int Cash;
-int cheat;
-int start;
-int l, j;
-int bet;
-char mast, rank; //переменные создаем для масти и ранга;
-int i;
-int cardRankUser[2]; //Одно из 13 значение номинала player
-int cardMastUser[2]; //Одно из 4 номиналов мастей player
-int StolRank[5]; //Те самые 5 карт на столе 
-int StolfinalSuit[5]; //Те самые 5 карт на столе 5 рангов и 5 значение 
-int main()
+main()
 {
-	setlocale(LC_ALL, "Rus");
-	int komb;
-	Cash = 100;
+	int bet;
+	int bank = 100;
+	int i;
+	int cardRank[5];
+	int cardSuit[5];
+	int	finalRank[5];
+	int finalSuit[5];
+	int ranksinHand[13];
+	int suitsinHand[4];
+	int winnings;
+	time_t t;
+	char suit, rank, stillPlay;
+	printGreeting();
+	do
+	{
+		bet = getBet();
+		srand(time(&t));
+		getFirstHand(cardRank, cardSuit);
+		printf("Твои 5 карт: \n");
+		for(i=0; i<5; i++)
+			{
+				suit = getSuit(cardSuit[i]);
+				rank = getRank(cardRank[i]);
+				printf("Карта №%d: %c%c\n", i+1, rank, suit );
+			}
+		for (i = 0; i < 4; ++i)
+		{
+			suitsinHand[i] = 0;
+		}
+		for (i = 0; i < 13; ++i)
+		{
+			ranksinHand[i] = 0;
+		}
+	getFinalHand(cardRank[], cardSuit[], finalRank[], finalSuit[], ranksinHand[], suitsinHand[]);
+	printf("Ваша последняя партия карт: \n");
+	for (int i = 0; i < 5; ++i)
+	{
+		suit = getSuit(finalSuit[i]);
+		rank = getRank(finalRank[i]);
+		printf("Карта №%d: %c%c\n", i+1, rank, suit );
+	}
+	winnings = analyzeHand(ranksinHand, suitsinHand);
+	printf("Вы выиграли %d!\n", bet*winnings );
+	bank = bank - bet + (bet*winnings);
+	printf("Хотите сыграть еще? \n");
+	scanf_s("%c", &stillPlay);
+	} while (toupper(stillPlay) == 'Y');
+	return;
+}	
+void printGreeting()
+{
 	printf(".......................................................................................................................\n");
 	printf("..              Приветствую тебя в моей ахрененной игре под названием   ARKANA POKER EDITOR                          ..\n");
 	printf("..    Для начала я попробую расказать пару правил и механик а дальше ты сам решишь как в это играть, хорошо?         ..\n");
@@ -62,7 +106,6 @@ int main()
 	printf(".. увелечия ставки сможешь написав в поле ставка 777.                                                                ..\n");
 	printf(".. А и я забыл представиться меня зову  Copmuter Alex и я буду твоим противником                                     ..\n");
 	printf(".......................................................................................................................\n\v");
-	printf("Ты в любой момент можешь посмотреть комбинации написав в поле для ставки 777, попробуй...\n");
 	printf(" ____________________\n");
 	printf("|  Ставка - ");
 	scanf_s("%d", &komb);
@@ -81,9 +124,9 @@ int main()
 			}
 			case 2800070:
 
-				Cheat(cheat, Cash);
-				Cash += 1000;
-				printf("Cash  = %d\n", Cash);
+				Cheat(cheat, bank);
+				bank += 1000;
+				printf("bank  = %d\n", bank);
 				break;
 			}
 			break;
@@ -132,144 +175,222 @@ int main()
 		printf("|Так у нас есть 52 карты наминалом от 2-10 Валет = V Дама = D Король = K и Туз = T |\n");
 		printf("|Каждая из них имеет одну из 4 мастей. Буби = B Пики = P Черви = H и Крести = K    |\n");
 		printf("| Таким образом у карты будет 2 значения  BK -  B - Буби, K - Король все карты так |\n");
-		printf("| Ах да еще же есть 10 она будет обозначена как D                                  |\n");
+		printf("| 					ГЛАВНОЕ РАЗВЛЕКАЙСЯ ПОКА Я БУДУ МАЙНИТЬ                        |\n");
 		printf(" ----------------------------------------------------------------------------------\n");
-			do
-			{
-				bet = getBet();
-				getFirstHand(cardRankUser, cardMastUser);
-				
-				{
-					int l, j;
-					int cardDup;
-					for (l = 0; l < 5; l++)
-					{
-						cardDup = 0;
-						do
-						{
-							cardRankUser[l] = (rand() % 13);
-							cardMastUser[l] = (rand() % 4);
-							for (j = 0; j < l; j++)
-							{
-								if ((cardRankUser[i] == cardRankUser[j]) && (cardMastUser[i] == cardMastUser[j]))
-								{
-									cardDup = 1;
-								}
-							}
-						} while (cardDup == 1);
-					}
-				}
-
-				char getMast(int mast); //у Здесь понятно кейс для масти...
-				{
-					switch (mast)
-					{
-					case 0:
-						return ('P');
-					case 1:
-						return ('B');
-					case 2:
-						return ('K');
-					case 3:
-						return ('H');
-					}
-				}
-				char getRank(int rank);
-				{
-					switch (rank)
-					{
-					case 0:
-						return ('T');
-					case 1:
-						return ('2');
-					case 2:
-						return ('3');
-					case 3:
-						return ('4');
-					case 4:
-						return ('5');
-					case 5:
-						return ('6');
-					case 6:
-						return ('7');
-					case 7:
-						return ('8');
-					case 8:
-						return ('9');
-					case 9:
-						return ('10');
-					case 10:
-						return ('V');
-					case 11:
-						return ('D');
-					case 12:
-						return ('K');
-					}
-				}
-				int getBet()
-				{
-					int bet;
-					do  	//Попытаемся ввести начальную ставку 
-					{
-						printf("Введи начальную ставку\n");
-						printf(" ___________________ \n");
-						printf("|Ставка -  \n");
-						scanf_s("%d", &bet);
-						printf(" ___________________|\n");
-						if (bet>Cash||bet<0)
-						{
-							return(bet);
-						}
-						else if (bet == 0)
-						{
-							exit(1);
-						}
-						else
-						{
-							printf("Пожалуйста введи ставку не превышающую Cash и не меньше 0\n");
-							printf("0 для выхода из игры\n");
-						}						
-					} while ((bet <0 || bet>Cash));
-
-
-				}
-				printf("Твои 2 карты\n");
-				for (l = 0; l < 2; l++)
-				{
-					mast = getMast(cardMastUser[l]);
-					rank = getRank(cardRankUser[l]);
-					printf("Карта 1 %c%c\t Карта 2 %c%c", rank, mast);//Выдаем карты...
-				}
-				printf(" _____________\n");
-				printf("|     СТОЛ    |\n");
-				printf(" -------------\n");
-			}
-
-	
-}
-	else
-	{
-	if (start != 0)
-	{
-		printf("Ты ввел не правильно попробуй вновь.\n");
-		printf("|  1||0 -   ");
-		scanf_s("%d", &start);
-		printf(" ____________________|\n");
-	}
-	int x, bin;
-	for (x = 0; x < 40; x++)
-	{
-		printf("Delete fail %d\n", x);
-		for (bin = 0; bin < 20; bin++)
+		void getFirstHand(int cardRank[], int cardSuit[])
 		{
-			printf("Delete bin %d\n", bin);
+			int i, j;
+			int cardDup;
+			for (i=0; i<5; i++)
+			{
+				cardDup = 0;
+				do
+				{
+					cardSuit[i] = (rand() % 13);
+					cardSuit[i] =(rand() % 4);
+					for (int j = 0; j < i; ++i)
+					{
+						if ((cardRank[i] == cardRank[j]) && cardSuit[i] == cardSuit[j])
+						{
+							cardDup = 1;
+						}
+					}
+				}while(cardDup == 1);
+
+			}
+		}
+	char getSuit(int suit)
+		{
+			switch (suit)
+				{
+				case 0:
+				return ('P');
+				case 1:
+				return ('B');
+				case 2:
+				return ('K');
+				case 3:
+				return ('H');
+				}
+		}
+	char getRank(int rank)
+	{
+		switch (rank)
+			{
+				case 0:
+				return ('T');
+				case 1:
+				return ('2');
+				case 2:
+				return ('3');
+				case 3:
+				return ('4');
+				case 4:
+				return ('5');
+				case 5:
+				return ('6');
+				case 6:
+				return ('7');
+				case 7:
+				return ('8');
+				case 8:
+				return ('9');
+				case 9:
+				return ('10');
+				case 10:
+				return ('V');
+				case 11:
+				return ('D');
+				case 12:
+				return ('K');
+			}
+	}
+int getBet()
+{
+	int bet;
+	do
+	{
+		printf("Сколько ты желаешь поставить, вводи число от 1 - 20\n");
+		printf("Ну или введи 0 если хочешь выйти из игры... \n"); 
+		scanf_s("%d", &bet);
+		if (bet>=1 && bet <=20)
+		{	
+			return(ber);
+		}
+		else if (bet == 0)
+		{
+			exit(1);
+		}
+		else
+		{
+			printf("Пожалуйста введи ставку от 1 - 20 или 0 что бы выйти из игры\n");
+		}
+	}while ((bet < 0) || (bet > 20));
+}
+int analyzeHand(int ranksinHand[], int suitsinHand[])
+	{
+		int num_consec = 0;
+		int i, rank, suit;
+		int strainght = FALSE;
+		int flush = FALSE;
+		int four = FALSE;
+		int three = FALSE;
+		int pairs = 0;
+		for (int suit = 0; suit < 4 ; suit++)
+			if (suitsinHand[suit] == 5)
+				flush = TRUE;
+			rank = 0;
+			while (ranksinHand[rank] == 0)
+				rank++;
+			for(; rank < 13 && ranksinHand[rank]; rank++)
+				num_consec++;
+			if (num_consec == 5)
+			{
+				strainght = TRUE;
+			}
+		for (rank = 0; rank < 13 ; rank++)
+			{
+				if (ranksinHand[rank] == 4)
+					four = TRUE;
+				if (ranksinHand[rank] == 3)
+					three == TRUE;
+				if (ranksinHand[2] == 2)
+					pairs++;
+			}	
+			if (strainght && flush)
+			{
+				printf("Феш рояль \n\n");
+				return (20);
+			}
+			else if (four)
+			{
+				printf("Каре\n\n");
+				return (10);
+			}
+			else if (three && pairs == 1)
+			{
+				printf("Фуллхаус\n\n");
+				return (8);
+			}
+			else if (flush)
+			{
+				printf("Флеш \n\n");
+				return (5);
+			}
+			else if (strainght)
+			{
+				printf("Стрит \n\n");
+				return (4);
+			}
+			else if (three)
+			{
+				printf("Три одного типа \n\n");
+				return (3);
+			}
+			else if (pairs == 2)	
+			{
+				printf("Две пары\n\n");
+				return (2);
+			}
+			else if (pairs == 1)
+			{
+				printf("Пара \n\n");
+				return (0);
+			}
+			else
+			{
+				printf("Старшая карта\n\n");
+				return 0;
+			}
+	}
+	void getFinalHand(int cardRank[], int cardSuit[], int finalRank[], int finalSuit[], int ranksinHand[], int suitsinHand[]);
+	{
+		int i, j, cardDup;
+		char suit, rank, ans;
+		for (i = 0; i < 5 ; i++)
+		{
+			suit = getSuit(cardSuit[i]);
+			rank = getRank(cardRank[i]);
+			printf("ЖЕлаешь сохранить карту №%d: %c%c?\n", i+1, rank, suit);
+				printf("Пожалуйста ответь Y||N :\n");
+				scanf_s("%c", &ans);
+				if (toupper(ans) == 'Y')
+				{
+					finalRank[i] = cardRank [i];
+					finalSuit[i] = cardSuit[i];
+					ranksinHand[finalRank[i]]++;
+					ranksinHand[finalSuit[i]]++;
+					continue;
+				}
+				else if (toupper(ans) == 'N')
+				{
+					cardDup = 0
+					do
+					{
+						cardDup = 0 
+						finalRank[i] = (rand() % 13);
+						finalSuit[i] = (rand() % 4);
+						for (j=0; j<5; j++)
+						{
+							if((finalRank[i] == cardRank[j]) && (finalSuit[i] == cardSuit[j]))
+							{
+								cardDup = 1;
+							}
+						}
+						for (j = 0; j < i ; j++)
+						{
+							if ((finalRank[i] == cardRank[j]) && (finalSuit[i] == cardSuit[j]))
+							{
+								cardDup = 1;
+							}
+						}
+					}while (cardDup == 1);
+					ranksinHand[finalRank[i]]++;
+					suitsinHand[finalSuit[i]]++;
+				}	
 		}
 	}
-	printf("Досвидания тогда\n");
-	printf("Windows delete\n");
-
-	return 0;
-	}
-
-	return 0;
+return 0;
 }
+
+
